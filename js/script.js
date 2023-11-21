f47f0a9d31af8f9fe13cbc5c60a432f0905f7018
Max_Mosquito <w-2aa@mail.ru>
2023-11-21 13:11:27 +0300

    правки по списку

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
+//burger---------------------
+function burger() {
+
+   const burger = document.getElementById('burger');
+   const mobMenuClose = document.getElementById('close');
+   const mobMenu = document.getElementById('mob_menu');
+
+   burger.addEventListener('click', () => {
+      mobMenu.classList.add('active');
+   })
+   mobMenuClose.addEventListener('click', () => {
+      mobMenu.classList.remove('active');
+   })
+
+}
+
+burger()
+
+//slider------------------------
 const headerSlider = new Swiper('.header_slider', {
    slidesPerView: 1,
    loop: true,
@@ -25,26 +44,30 @@ const headerSlider = new Swiper('.header_slider', {
  })
 
  //tabs--------------------
- const tabBtn = document.querySelectorAll('.techology_tabs_headers_item');
- const tabContent = document.querySelectorAll('.techology_tabs_contemt_item');
+function tabs() {
 
-function tabReset() {
-   tabBtn.forEach(item => {
-      item.classList.remove('active');
-   })
-
-   tabContent.forEach(item => {
-      item.classList.remove('active');
+   const tabBtn = document.querySelectorAll('.techology_tabs_headers_item');
+   const tabContent = document.querySelectorAll('.techology_tabs_contemt_item');
+   
+   function tabReset() {
+      tabBtn.forEach(item => {
+         item.classList.remove('active');
+      })
+   
+      tabContent.forEach(item => {
+         item.classList.remove('active');
+      })
+   }
+   
+   tabBtn.forEach((item, i) => {
+      item.addEventListener('click', () => {
+         tabReset()
+   
+         item.classList.add('active');
+   
+         tabContent[i].classList.add('active');
+      })
    })
 }
 
- tabBtn.forEach((item, i) => {
-   item.addEventListener('click', () => {
-      tabReset()
-
-      item.classList.add('active');
-
-      tabContent[i].classList.add('active');
-   })
- })
- 
+tabs()
